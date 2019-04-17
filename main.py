b548aab26d3e7f625be5e3109c36bfd52b2aa716
amirkhan1092 <amirkhan1092@gmail.com>
2019-04-17 23:16:23 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+a = open('sectionJ.dat','r')
+
+data = a.readlines()
+data_t = []
+for k in data:
+    data_t.append(tuple(k.split('\t')))
+
+
+data_dict = dict(data_t)
+
+name = input('enter the name of the Student ')
+
+search = data_dict.get(name.upper(),'Not found')
+if search != 'Not found':
+    print('Name found')
+    print(name.upper()+' +91'+search)
+else:
+    print(search)
+
+
+a.close()
