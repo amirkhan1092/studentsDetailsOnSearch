46593de99d65e1f304426ae87b298ff7ca2b1c77
amirkhan1092 <amirkhan1092@gmail.com>
2019-04-17 23:55:02 +0530

    all student details file added

---
+++ README.md
@@ -0,0 +1,15 @@
+# simple file handling based text file searching name 
+
+Welcome to the file handling based Student Details Search 
+
+Select the Category 
+1. MCA
+2. Btech Section E
+3. Btech Section J
+4. Btech Section X
+
+
+enter the choices 1/2/3/4 1
+enter the full name of the Student krati verma
+Name found
+KRATI VERMA +91xxxxxxxxx
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 
+print('Welcome to the file handling based Student Details Search ')
+print('''
+Select the Category 
+1. MCA
+2. Btech Section E
+3. Btech Section J
+4. Btech Section X
 
-a = open('sectionE.dat','r')
+''')
+c = input('enter the choices 1/2/3/4 ')
+
+if c == '1':
+    file = 'mca1st.dat'
+elif c == '2':
+    file = 'sectionE.dat'
+elif c == '3':
+    file = 'sectionJ.dat'
+elif c == '4':
+    file = 'sectionX.dat'
+
+
+
+a = open(file,'r')
 
 data = a.readlines()
 data_t = []
 for k in data:
-    data_t.append(tuple(k.split('\t')))
-
+    data_t.append(k.split('\t'))
 
 data_dict = dict(data_t)
 
-name = input('enter the name of the Student ')
+name = input('enter the full name of the Student ')
 
-search = data_dict.get(name.upper(),'Not found')
-if search != 'Not found':
+search = data_dict.get(name.upper())
+if search:
     print('Name found')
     print(name.upper()+' +91'+search)
 else:
